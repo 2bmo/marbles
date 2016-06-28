211f1e1c979f69aa87d815f83972f38f081734b4
Valerie V <heyfilky@gmail.com>
2016-06-28 14:18:51 +0300

    browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,8 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    gulpIf = require('gulp-if'),
-    cssnano = require('gulp-cssnano'),
-    uglify = require('gulp-uglify'),
     useref = require('gulp-useref'),
     autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create(),
     runSequence = require('run-sequence');
 
 gulp.task('sass', function() {
@@ -12,26 +10,34 @@ gulp.task('sass', function() {
         .pipe(sass({
             errLogToConsole: true
         }))
-        .pipe(gulp.dest('res'))
+        .pipe(gulp.dest('res/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
-gulp.task('watch', function () {
+gulp.task('watch', ['browserSync', 'sass'], function () {
     gulp.watch('scss/**/*.scss', ['sass']);
 });
 gulp.task('useref', function(){
     return gulp.src('res/*.html')
         .pipe(useref())
-        .pipe(gulpIf('*.js', uglify()))
-        .pipe(gulpIf('*.css', cssnano()))
         .pipe(gulp.dest('res'))
 });
 gulp.task('auto', function () {
-    return gulp.src('res/*.css')
+    return gulp.src('scss/css/**/*.css')
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest('res'));
+        .pipe(gulp.dest('res/css'));
 });
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'res'
+        },
+    })
+})
 gulp.task('default', function (callback) {
     runSequence(['sass', 'auto', 'useref', 'watch'],
         callback
